5ea7ca0bd650bed9fa90459e770be005d5f71a91
Cheng Zhao <zcbenz@gmail.com>
2023-12-03 17:17:48 +0900

    Test the source tarball

--- run_hooks.py
+++ run_hooks.py
@@ -23,9 +23,26 @@ def download_from_google_storage(
     args += [ '-o', output ]
   subprocess.check_call(args)
 
-def download_to(url, target):
-  if not os.path.exists(target):
-    urllib.request.urlretrieve(url, target)
+def cipd(root, ensure):
+  args = [ 'cipd', 'ensure', '-root', root, '-ensure-file', '-' ]
+  process = subprocess.Popen(args,
+                             text=True,
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+  stdout, stderr = process.communicate(input=ensure)
+  if process.returncode != 0:
+    print(stdout)
+    print(stderr)
+    raise ValueError('cipd failed.')
+
+def read_var_from_deps(var):
+  with open('src/DEPS', 'r') as file:
+    content = file.read()
+    prefix = f"'{var}': '"
+    start = content.find(prefix) + len(prefix)
+    end = content.find("'", start)
+    return content[start:end]
 
 def main():
   add_depot_tools_to_path()
@@ -36,15 +53,23 @@ def main():
                           'src/build/util/LASTCHANGE' ])
   subprocess.check_call([ sys.executable,
                           'src/build/util/lastchange.py',
+                          '-m', 'GPU_LISTS_VERSION',
+                          '--revision-id-only',
+                          '--header', 'src/gpu/config/gpu_lists_version.h' ])
+  subprocess.check_call([ sys.executable,
+                          'src/build/util/lastchange.py',
+                          '-m', 'SKIA_COMMIT_HASH',
+                          '-s', 'src/third_party/skia',
+                          '--header', 'src/skia/ext/skia_commit_hash.h' ])
+  subprocess.check_call([ sys.executable,
+                          'src/build/util/lastchange.py',
                           '-s', 'src/third_party/dawn',
                           '--revision', 'src/gpu/webgpu/DAWN_VERSION' ])
 
-  download_to('https://chrome-infra-packages.appspot.com/dl/gn/gn/linux-amd64/+/latest',
-              'src/buildtools/linux64/gn')
-  download_to('https://chrome-infra-packages.appspot.com/dl/gn/gn/mac-amd64/+/latest',
-              'src/buildtools/mac/gn')
-  download_to('https://chrome-infra-packages.appspot.com/dl/gn/gn/windows-amd64/+/latest',
-              'src/buildtools/win/gn.exe')
+  gn_version = read_var_from_deps('gn_version')
+  cipd('src/buildtools/mac', 'gn/gn/mac-amd64 ' + gn_version)
+  cipd('src/buildtools/win', 'gn/gn/windows-amd64 ' + gn_version)
+  cipd('src/buildtools/linux64', 'gn/gn/linux-amd64 ' + gn_version)
 
   download_from_google_storage(
       'chromium-nodejs',
